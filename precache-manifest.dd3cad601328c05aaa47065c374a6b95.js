9972767a3693311637e371461d6b0e3e4d743684
Simuk Arya <simukarya@simuks-air.home>
2020-04-25 08:19:35 -0400

    Updates

---
+++ precache-manifest.dd3cad601328c05aaa47065c374a6b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1d1bd9e68e703190aa6ca35ec01d9e",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/Robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "89a60b67707a5e40bca5",
+    "url": "/Robofriends/static/css/main.49ea09a4.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a60b67707a5e40bca5",
+    "url": "/Robofriends/static/js/main.1b62de64.chunk.js"
+  },
+  {
+    "revision": "d53e560d43c7171e8016",
+    "url": "/Robofriends/static/js/runtime-main.e0286ea6.js"
+  }
+]);
